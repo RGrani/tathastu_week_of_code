47de247d34d718442bb20d4f04682dc70f3579d6
RGrani <65910699+RGrani@users.noreply.github.com>
2020-05-28 23:54:05 +0530

    Create program1.py

---
+++ day3/program1.py
@@ -0,0 +1,4 @@
+#reverse of string
+str = input("enter string : ")
+print("orignal string is :- \n ","                 ",str)
+print("reverse of the string is :-\n","                         ",str[::-1])
